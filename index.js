f01255a545c0b8f27624f2fb6c05ca46de3fa330
relegated <35969849+relegated@users.noreply.github.com>
2020-05-21 10:50:30 -0600

    initial push

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.render('public/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
