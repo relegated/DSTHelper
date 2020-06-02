0e03de9522d8d947fce92f859ffe08af37249803
relegated <35969849+relegated@users.noreply.github.com>
2020-06-02 16:10:27 -0600

    Update contact.js

--- public/contact.js
+++ public/contact.js
@@ -17,7 +17,7 @@ $(document).ready( () => {
 });
 
 function BuildApiString() {
-    return `https://developers.google.com/civic-information/docs/v2/representatives/representativeInfoByAddress?apix_params=`
+    return `https://developers.google.com/civic-information/docs/v2/representatives/representativeInfoByAddress?key=AIzaSyDOoCjpX2_v8KmJonjuQHLwCLxBCk8MNK4&address=`
     + `{"${$("#address").val()} ${$("#city").val()} ${$("#state").value} ${$("#zip").val()}"`
     + `,"includeOffices":true, "roles":["legislatorLowerBody","legislatorUpperBody"]}`; 
 }
